77781fab2ae5f3e24944b22bd627a985adbeeab1
amira_nasser <amira20nasser@gmail.com>
2024-05-05 01:05:36 +0300

    state management session change notifier

--- Fourth Session/hive_session/android/app/src/main/kotlin/com/example/hive_session/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.hive_session
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- Fourth Session/hive_session/lib/constants.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/widgets.dart';
-
-List<Color> kColors = const [
-  Color(0xff092635),
-  Color(0xff1B4242),
-  Color(0xff5C8374),
-  Color(0xffC69774),
-  Color(0xFF944E63),
-];
